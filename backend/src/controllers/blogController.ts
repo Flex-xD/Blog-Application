ef4b884f9eea415f4397e97c8f0c02c17450a935
Flex-xD <bishtaman88102@gmail.com>
2025-08-08 16:45:42 +0530

    Now createBlog controller is working , and blogs are getting saved

--- backend/src/controllers/blogController.ts
+++ backend/src/controllers/blogController.ts
@@ -4,6 +4,7 @@ import { StatusCodes } from "http-status-codes";
 import User, { IUser, ZuserSchema, zUserType } from "../models/userModel";
 import Blog, { IBlog } from "../models/blogModel";
 import mongoose from "mongoose";
+import { success } from "zod";
 
 interface IAuthRequest extends Request {
     userId?: string
@@ -42,7 +43,7 @@ export const createBlogController = async (req: IAuthRequest, res: Response) => 
                 _id: (user._id as mongoose.Types.ObjectId).toString()
             });
             if (!safeUser.success) {
-                return  sendResponse(res, {
+                return sendResponse(res, {
                     statusCode: 400,
                     success: false,
                     msg: "Invalid user data",
@@ -59,23 +60,24 @@ export const createBlogController = async (req: IAuthRequest, res: Response) => 
             await userBlog.save({ session });
             user.userBlogs.push(userBlog._id as unknown as mongoose.Types.ObjectId);
             await user.save({ session });
-            return   sendResponse(res, { statusCode: StatusCodes.CREATED, success: true, msg: "Blog successfully created !", data: userBlog })
+            await session.commitTransaction();
+            return sendResponse(res, { statusCode: StatusCodes.CREATED, success: true, msg: "Blog successfully created !", data: userBlog })
         } catch (error) {
             session.abortTransaction();
-            sendError(res, { statusCode: StatusCodes.INTERNAL_SERVER_ERROR, error })
+            return sendError(res, { statusCode: StatusCodes.INTERNAL_SERVER_ERROR, error })
         } finally {
             session.endSession();
         }
     } catch (error) {
-        sendError(res, { error })
+        return sendError(res, { error })
     }
 }
 
-export const getUserBlogs = async (req:IAuthRequest, res: Response) => {
+export const getUserBlogs = async (req: IAuthRequest, res: Response) => {
     try {
         const { userId } = req;
         if (!userId) {
-            sendResponse(res, { statusCode: StatusCodes.UNAUTHORIZED, success: false, msg: "You are unauthorized !" })
+            return sendResponse(res, { statusCode: StatusCodes.UNAUTHORIZED, success: false, msg: "You are unauthorized !" })
         }
         const user: IUser | null = await User.findById(userId).populate("userBlogs");
         if (!user) {
@@ -85,37 +87,80 @@ export const getUserBlogs = async (req:IAuthRequest, res: Response) => {
                 msg: "User not found",
             });
         }
-        const safeUser = ZuserSchema.safeParse({
-            ...user.toObject(),
-            _id: (user._id as mongoose.Types.ObjectId).toString()
-        });
-        if (!safeUser.success) {
-            return sendResponse(res, {
-                statusCode: 400,
-                success: false,
-                msg: "Invalid user data",
-                data: safeUser.error.issues,
-            });
-        }
-
-        const validUser = safeUser.data;
-        return sendResponse(res , {
-            statusCode:StatusCodes.OK , 
-            success:true , 
-            msg:"User Blogs found !" , 
-            data:validUser.userBlogs as unknown as IBlog[]
+        return sendResponse(res, {
+            statusCode: StatusCodes.OK,
+            success: true,
+            msg: "User Blogs found !",
+            data: user.userBlogs as unknown as IBlog[]
         })
     } catch (error) {
-        sendError(res , {error});
+        sendError(res, { error });
     }
 }
 
-export const getUserSavedBlogs = async () => {
+export const getUserSavedBlogs = async (req: IAuthRequest, res: Response) => {
+    try {
+        const { userId } = req;
+        if (!userId) {
+            return sendResponse(res, { statusCode: StatusCodes.UNAUTHORIZED, success: false, msg: "You are unauthorized !" })
+        }
+        const user: IUser | null = await User.findById(userId).populate("userBlogs");
+        if (!user) {
+            return sendResponse(res, {
+                statusCode: StatusCodes.NOT_FOUND,
+                success: false,
+                msg: "User not found",
+            });
+        }
 
+        const savedBlogs = user.saves as unknown as IBlog[]
+        return sendResponse(res, {
+            statusCode: StatusCodes.OK,
+            success: true,
+            msg: savedBlogs.length ? "User Blogs found !" : "You haven't saved any blogs yet !",
+            data: savedBlogs
+        })
+    } catch (error) {
+        sendError(res, { error });
+    }
 }
 
-export const getUserFeed = async () => {
+export const getUserFeed = async (req: IAuthRequest, res: Response) => {
+    try {
+        // * GET USER FEED
+        // ? Check wheter user is there or not 
+        const { userId } = req;
+        if (!userId) {
+            return sendResponse(res, { statusCode: StatusCodes.UNAUTHORIZED, success: false, msg: "You are unauthorized !" })
+        }
+        const user: IUser | null = await User.findById(userId).populate("userBlogs");
+        if (!user) {
+            return sendResponse(res, {
+                statusCode: StatusCodes.NOT_FOUND,
+                success: false,
+                msg: "User not found",
+            });
+        }
 
+        if (user.following.length) {
+            return sendResponse(res , {
+                statusCode:StatusCodes.LENGTH_REQUIRED , 
+                success:false , 
+                msg:"User don't have any following !" , 
+            })
+        }
+        for (let i = 0 ; i < user.following.length ; i++) {
+            const userFollowing : IUser | null = await User.findById(i);
+            userFollowing?.userBlogs
+        }
+        user.following
+        // ? Now Check if User's following have uploaded , something ( let followingUploaded )
+        // ? If followingUploaded true and user havn't seen them then show followings blogs and 
+        // ? and then show the latest blogs all over 
+        // ? If followingUploaded false , just show the latest blogs 
+    } catch (error) {
+        sendError(res, { error });
+    }
 }
 
 export const deleteUserBlog = async () => {
--- backend/src/routes/blogRoutes.ts
+++ backend/src/routes/blogRoutes.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import verifyToken from "../middleware/authMiddleware";
-import { createBlogController, getUserBlogs } from "../controllers/blogController";
+import { createBlogController, getUserBlogs, getUserSavedBlogs } from "../controllers/blogController";
 
 const blogRouter = Router();
 
+// ? USER'S FEED
 blogRouter.get("/get-user-feed" , verifyToken , getUserBlogs);
-blogRouter.post("/create" , verifyToken , createBlogController);
+
+// ? USER'S BLOGS
 blogRouter.get("/user-blogs" , verifyToken , getUserBlogs);
+blogRouter.get("/user-saved-blogs" , verifyToken , getUserSavedBlogs)
+
+// ? POST BLOG
+blogRouter.post("/create" , verifyToken , createBlogController);
+
 
 export default blogRouter;
